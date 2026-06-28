7d5bdd91bd9db37c1894e343bbdd930cbd54808d
Django <django@hermes>
2026-06-27 22:28:11 -0400

    Fix upload URL - should be /upload/ not /upload/upload/

--- files/upload_urls.py
+++ files/upload_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import upload_views
 
 urlpatterns = [
-    path('upload/', upload_views.upload_page, name='upload_page'),
+    path('', upload_views.upload_page, name='upload_page'),
     path('api/upload/', upload_views.upload_document, name='upload_document'),
 ]
